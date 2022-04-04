5b1ca90435393e058ff46978d7f20988e1bb00b0
Alba <77460867+Alba233@users.noreply.github.com>
2022-04-04 17:56:31 +0200

    monday responsive / burger
    pas très efficace sur le responsive
    burger ne fonctionne pas...

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-list');
+    const navLinks = document.querySelectorAll('.nav-list li');
+
+    burger.addEventListener('click',()=>{
+        //toggle nav
+        nav.classList.toggle('nav-active');
+        
+        //animation links
+        navLinks.forEach((links,index) => {
+            if (links.style.animation) {
+                links.style.animation = '';
+            } else {
+                links.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 1.5}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
